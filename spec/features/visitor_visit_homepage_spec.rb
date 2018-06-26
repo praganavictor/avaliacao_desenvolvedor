0b7d657c5738dd40b8435c573a18a41002f2d399
Victor Pragana <victormerigue@gmail.com>
2018-06-26 19:40:22 -0300

    create controller home

---
+++ spec/features/visitor_visit_homepage_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'visitor visit homepage' do
+  scenario 'successfully' do
+    visit root_path
+
+    expect(page).to have_css('h1', text: 'Avaliação de desenvolvedor')
+    
+  end
+end
